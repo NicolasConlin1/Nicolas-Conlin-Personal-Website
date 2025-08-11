70e8917ff8a54d4d8479d7cc833364d315cf90f7
NicolasConlin <nicoconlin@gmail.com>
2025-08-10 21:04:14 -0600

    Added  lines for github deployment

--- reactjs/vite.config.js
+++ reactjs/vite.config.js
@@ -4,11 +4,12 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    server: {
-        port: 55095,
-        //Allows ngrok for testing.
-        //ngrok http http://localhost:55095
-        //allowedHosts: true,
-        allowedHosts: false,
-    }
+    base: '/Nicolas-Conlin-Personal-Website',
+    //server: {
+    //    //port: 55095,
+    //    //Allows ngrok for testing.
+    //    //ngrok http http://localhost:55095
+    //    //allowedHosts: true,
+    //    allowedHosts: false,
+    //}
 })
